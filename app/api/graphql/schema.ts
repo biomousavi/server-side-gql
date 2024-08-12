7d2f964ed2a86b262dfdf89056808a9cf464cf02
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-08-12 15:10:16 +0330

    feat: add resolver for __resolveType

--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,7 +1,20 @@
 const schema = `#graphql
 
+  type Person {
+    id:ID!
+    name:String!
+  }
+
+  type Animal {
+    id:String!
+    species:String!
+  }
+
+  union SearchType = Animal | Person
+
   type Query {
     me: String!
+    search: [SearchType]!
   }
 
 `
