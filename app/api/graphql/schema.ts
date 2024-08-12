72a0fe61898e093858e9b19029d042b9e3e07802
Mohammad Hosein Mousavi <biomousavi@gmail.com>
2024-08-12 09:26:16 +0330

    chore: add interface

--- app/api/graphql/schema.ts
+++ app/api/graphql/schema.ts
@@ -1,11 +1,30 @@
 const schema = `#graphql
 
-  type Person {
+  interface Character {
     name: String!
+    outfit: String!
+    strengthStat: Int!
+  }
+
+  type Person implements Character {
+    name: String!
+    outfit: String!
+    strengthStat: Int!
+
+    background: String!
+  }
+
+  type Alien implements Character {
+    name: String!
+    outfit: String!
+    strengthStat: Int!
+
+    homeWorld: String!
   }
 
   type Query {
     me: Person!
+    characters: [Alien!]!
   }
 
 `
